122975d9e5c7a74f177c3acde8803e91a979ba3f
Jeremy Bowman <jbowman@safaribooksonline.com>
2015-02-12 17:45:46 -0500

    More packaging updates, fixed tests on Django 1.7

--- yet_another_django_profiler/tests/test_django_settings.py
+++ yet_another_django_profiler/tests/test_django_settings.py
@@ -9,7 +9,7 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.utils import override_settings
 
-ADMIN_EXCERPT = 'Django administration'
+TEST_PAGE_EXCERPT = 'This is only a test'
 HELP_EXCERPT = 'profiling middleware'
 
 
@@ -17,31 +17,31 @@ class DjangoSettingsTest(TestCase):
 
     def test_no_profiling(self):
         """Views should work normally when the profiler is not specifically invoked"""
-        response = self._get_admin()
-        self.assertContains(response, ADMIN_EXCERPT)
+        response = self._get_test_page()
+        self.assertContains(response, TEST_PAGE_EXCERPT)
 
     @override_settings(YADP_ENABLED=True)
     def test_profiler_enabled(self):
         """The profiler should be usable when YADP_ENABLED is True"""
-        response = self._get_admin('profile=help')
-        self.assertNotContains(response, ADMIN_EXCERPT)
+        response = self._get_test_page('profile=help')
+        self.assertNotContains(response, TEST_PAGE_EXCERPT)
         self.assertContains(response, HELP_EXCERPT)
 
     def test_obeys_debug(self):
         """The middleware should be disabled when DEBUG = False and there is no explicit YADP_ENABLED"""
-        response = self._get_admin('profile=help')
-        self.assertContains(response, ADMIN_EXCERPT)
+        response = self._get_test_page('profile=help')
+        self.assertContains(response, TEST_PAGE_EXCERPT)
 
     @override_settings(YADP_ENABLED=True, YADP_PROFILE_PARAMETER='other')
     def test_alternate_profile_parameter(self):
         """It should be possible to override the profile parameter name"""
-        response = self._get_admin('profile=help')
-        self.assertContains(response, ADMIN_EXCERPT)
-        response = self._get_admin('other=help')
+        response = self._get_test_page('profile=help')
+        self.assertContains(response, TEST_PAGE_EXCERPT)
+        response = self._get_test_page('other=help')
         self.assertContains(response, HELP_EXCERPT)
 
-    def _get_admin(self, params=''):
-        url = reverse('admin:index')
+    def _get_test_page(self, params=''):
+        url = reverse('test')
         if params:
             url += '?' + params
         return self.client.get(url)
