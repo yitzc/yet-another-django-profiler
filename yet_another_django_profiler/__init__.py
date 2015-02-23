8368f3da0f3f548114edd82040f3efdfa195bbd2
Jeremy Bowman <jmbowman@alum.mit.edu>
2015-02-23 16:34:20 -0500

    Clarify licensing

--- yet_another_django_profiler/__init__.py
+++ yet_another_django_profiler/__init__.py
@@ -1,6 +1,9 @@
 # encoding: utf-8
 # Created by Jeremy Bowman on Thu Feb 20 16:55:15 EST 2014
-# Copyright (c) 2014 Safari Books Online. All rights reserved.
+# Copyright (c) 2014, 2015 Safari Books Online. All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the 3-clause BSD license.  See the LICENSE file for details.
 """
 Yet Another Django Profiler middleware
 
--- yet_another_django_profiler/tests/test_django_settings.py
+++ yet_another_django_profiler/tests/test_django_settings.py
@@ -1,6 +1,9 @@
 # encoding: utf-8
 # Created by Jeremy Bowman on Fri Feb 21 16:06:03 EST 2014
-# Copyright (c) 2014 Safari Books Online. All rights reserved.
+# Copyright (c) 2014, 2015 Safari Books Online. All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the 3-clause BSD license.  See the LICENSE file for details.
 """
 Yet Another Django Profiler settings tests
 """
